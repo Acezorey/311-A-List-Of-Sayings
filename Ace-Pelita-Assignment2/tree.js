fe50c74a31843091d24cac5ed7ccc0ddf09efa2b
Acezorey <ace.reyes0213@gmail.com>
2024-09-24 22:53:38 -1000

    Update tree.js

--- Ace-Pelita-Assignment2/tree.js
+++ Ace-Pelita-Assignment2/tree.js
@@ -1,11 +1,27 @@
-//Implemented by Ace
+//Implemented by Ace and Pelita
+
+/*
+This is the blueprint of the nodes in the tree
+Six parameters:
+- japanese: Japanese saying
+- english: English translation of saying
+- englishex: English explanation
+- japanex: Japanese (Romanji) translation of Saying
+*/
 class Saying {
-    constructor(english, japanese, englishex, japanex){
-        this.english = english;
+    constructor(japanese, english, englishex, japanex){
         this.japanese = japanese;
+        this.english = english;
         this.englishex = englishex;
         this.japanex = japanex;
+        this.right = null;
+        this.left = null;
     }
 }
+
+const root = null;
+
+const englishList = [];
+const japaneseList = [];
 
 
