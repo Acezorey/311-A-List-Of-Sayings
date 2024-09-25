7e91d457706f69b7fa785c5faa9eb4e24e0fa036
Acezorey <ace.reyes0213@gmail.com>
2024-09-24 22:43:58 -1000

    Switched to javascript

---
+++ Ace-Pelita-Assignment2/tree.js
@@ -0,0 +1,11 @@
+//Implemented by Ace
+class Saying {
+    constructor(english, japanese, englishex, japanex){
+        this.english = english;
+        this.japanese = japanese;
+        this.englishex = englishex;
+        this.japanex = japanex;
+    }
+}
+
+
