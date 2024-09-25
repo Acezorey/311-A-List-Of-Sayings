d96417eee2cd8e771c89468fad3d0ea0d0108498
Acezorey <ace.reyes0213@gmail.com>
2024-09-24 22:31:41 -1000

    Created assignment

---
+++ Ace-Pelita-Assignment2/tree.ts
@@ -0,0 +1,26 @@
+//Implemented by Ace Reyes
+
+class Saying {
+    english: string;
+    japanese: string;
+    englishExplanation: string;
+    japaneseExplanation: string;
+    public constructor(english: string, japanese: string, eExplan: string, jExplan: string){
+        this.english = english;
+        this.japanese = japanese;
+        this.englishExplanation = eExplan;
+        this.japaneseExplanation = jExplan;
+    }
+}
+
+const meHua: Saying[] = [];
+const withWord: Saying[] = [];
+
+function insert(saying: Saying[]){
+
+}
+
+
+
+
+//Implemented by Pelita Felicitas
